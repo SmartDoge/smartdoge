bb06e8d992591454e2e01b0b00c527c66232aad6
Vitalik Wooferin <vitalik@smartdoge.com>
2022-06-14 11:21:04 -0700

    fix: update paths

--- x/evm/migrations/v2/migrate_test.go
+++ x/evm/migrations/v2/migrate_test.go
@@ -10,12 +10,12 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	paramtypes "github.com/cosmos/cosmos-sdk/x/params/types"
 
-	"github.com/tharsis/ethermint/encoding"
+	"github.com/SmartDoge/smartdoge/encoding"
 
-	"github.com/tharsis/ethermint/app"
-	v2 "github.com/tharsis/ethermint/x/evm/migrations/v2"
-	v2types "github.com/tharsis/ethermint/x/evm/migrations/v2/types"
-	"github.com/tharsis/ethermint/x/evm/types"
+	"github.com/SmartDoge/smartdoge/app"
+	v2 "github.com/SmartDoge/smartdoge/x/evm/migrations/v2"
+	v2types "github.com/SmartDoge/smartdoge/x/evm/migrations/v2/types"
+	"github.com/SmartDoge/smartdoge/x/evm/types"
 )
 
 func TestMigrateStore(t *testing.T) {
--- x/evm/migrations/v2/types/chain_config.go
+++ x/evm/migrations/v2/types/chain_config.go
@@ -10,7 +10,7 @@ import (
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/params"
 
-	"github.com/tharsis/ethermint/x/evm/types"
+	"github.com/SmartDoge/smartdoge/x/evm/types"
 )
 
 // EthereumConfig returns an Ethereum ChainConfig for EVM state transitions.
